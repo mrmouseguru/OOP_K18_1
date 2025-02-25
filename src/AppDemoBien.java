03df1a1af2fe99b10354bc8a109b832c7fd60dbc
CGM <mrmouse.guru@gmail.com>
2025-02-25 14:31:25 +0700

    Minh họa Biến cơ bản (primitive) và Biến tham chiếu đối tượng

---
+++ src/AppDemoBien.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class AppDemoBien {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int soNguyen = 4;
+		
+		//khai báo biến tham chiếu đối tượng
+		//có tên hcn
+		//có kiểu là kiểu dữ liệu tham chiếu đối tượng
+		//HinhChuNhat
+		HinhChuNhat hcn;
+		//khởi tạo đối tượng
+		hcn = new HinhChuNhat();
+		
+		HinhChuNhat hcn2 = new HinhChuNhat(10, 5);
+		
+		//3 bước để khởi tạo và dùng object
+		//Bước 1
+		HinhChuNhat hcn3 = null;
+		//Bước 2
+		//khởi tạo đối tượng trong vùng nhớ Heap
+		hcn = new HinhChuNhat();
+		//bước 3 liên kết biến tham chiếu đối tượng
+		//đến đối tượng đã được tạo trong Heap
+		//hcn = 
+		//
+		Scanner keyboardInput;
+		//bước 2
+		new Scanner(System.in);
+		//Bước 3
+
+	}
+
+}
